f578ef64e96af58641957597ba99736b6010de5f
Wesley Farias Goes <wfariasgoes@gmail.comr>
2023-03-03 09:33:07 -0300

    Create flavors admin and client for whitelabel

---
+++ app/src/main/java/com/br/whitelabel/ui/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.br.whitelabel.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
+import com.br.whitelabel.R
+import com.br.whitelabel.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+
+        navController = navHostFragment.navController
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+
+        binding.toolbarMain.setupWithNavController(navController, appBarConfiguration)
+    }
+}
---
+++ app/src/main/java/com/br/whitelabel/util/Constants.kt
@@ -0,0 +1,6 @@
+package com.br.whitelabel.util
+
+const val COLLECTION_ROOT = "data"
+const val COLLECTION_PRODUCTS = "products"
+const val STORAGE_IMAGES = "images"
+const val PRODUCT_KEY = "product"
